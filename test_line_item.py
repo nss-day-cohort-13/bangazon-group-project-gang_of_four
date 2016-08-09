bf89a19fa4546f5c236e83fb03d69a7e631d91ba
Amari Ashwood <fromashestosnow@gmail.com>
2016-08-09 11:26:01 -0500

    tests pass for line_item

--- test_line_item.py
+++ test_line_item.py
@@ -1,4 +1,6 @@
 import unittest
+from product import *
+from payment import *
 from line_item import *
 from order import *
 from customer import *
@@ -12,20 +14,25 @@ class TestLineItem(unittest.TestCase):
 
 # ******************* LINE ITEM ****************************
   def test_new_line_item(self):
-    order = Order(customer.cust_uuid, payment.pay_uuid)
+    customer = Customer("Billy Bob", "123 Melbourne", "Hville", "VA", "12345", "123-456-7890")
+    order = Order(customer.cust_uuid, pay_uuid=None)
+    payment = Payment("Goats", "2")
+    product = Product("CraftBeer", "6")
     line_item = Line_Item(
-              product.uuid,
-              order_uuid
+              product.product_uuid,
+              order.order_uuid
               )
 
-    #not sure if this is written correctly (below)
     self.assertEqual(line_item.order_uuid, order.order_uuid)
-    self.assertEqual(line_item.pay_uuid, None)
-    self.assertEqual(line_item.order_is_open, True)
+    self.assertEqual(order.order_is_open, True)
     self.assertIsInstance(line_item, Line_Item)
-    self.assertIsNotNone(line_item.cust_uuid)
-    self.assertIsNone(line_item.pay_uuid)
+    self.assertIsNotNone(payment.pay_uuid)
+    self.assertIsNotNone(customer.cust_uuid)
     self.assertIsNotNone(line_item.line_item_uuid)
+    self.assertIsNotNone(order.order_uuid)
+    self.assertIsNotNone(payment.pay_uuid)
+    self.assertIsNotNone(customer.cust_uuid)
+    self.assertIsNotNone(product.product_uuid)
 
 if __name__ == '__main__':
   unittest.main()
