0a3660648f825496ebe01149dfc944ce399a8849
Amari Ashwood <fromashestosnow@gmail.com>
2016-08-09 15:51:31 -0500

    fixes for requested things on product logic

--- bangazon.py
+++ bangazon.py
@@ -122,10 +122,9 @@ class Bangazon():
     print("\nLet's add a product")
     add_product = input('Enter Product Name: ')
     add_product_price = input('Enter Product Price: ')
-    new_product = New_Product(add_product, add_product_price)
-    print('New Product Creation: ', new_product.product_uuid)
+    new_product = Product(add_product, add_product_price)
     self.all_products[new_product.product_uuid] = new_product
-
+    print("Your product was created.")
     self.serialize_data(self.all_products, 'products.p')
     time.sleep(5)
     pass
