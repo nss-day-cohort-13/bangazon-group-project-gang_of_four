39662b46a3aa4528d5634e14437f0d29e447cf60
Simon Herrera <simonherrera.tech@gmail.com>
2016-08-17 15:19:52 -0500

    Update Payment SQL

--- table.py
+++ table.py
@@ -1,7 +1,6 @@
 import sys
 import sqlite3
 from datetime import datetime
-from time import time
 
 class BangTable:
   '''create class for customer table'''
@@ -21,6 +20,25 @@ class BangTable:
 
       k.execute("insert into customer values (?, ?, ?, ?, ?, ?)",
                     (new_customer.name, new_customer.address, new_customer.city, new_customer.state, new_customer.postal_code, new_customer.phone_number))
+
+      khan.commit()
+
+
+  def payment_table(new_payment):
+    '''Puts Customer Data in a Table
+    '''
+    with sqlite3.connect('bangazon.db') as khan:
+      k = khan.cursor()
+
+      try:
+        k.execute("""create table payment
+          (payment_name, payment_account_number)""")
+      except sqlite3.OperationalError:
+        pass
+
+
+      k.execute("insert into payment values (?, ?)",
+                    (new_payment.payment_name, new_payment.payment_account_number))
 
 
       khan.commit()
