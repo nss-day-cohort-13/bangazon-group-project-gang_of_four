6a6afef36d834dea53d22f4a5a90779eff758a63
Keith <keithbradley1@msn.com>
2016-08-18 14:14:17 -0500

    commiting so I can push and see previous commit

--- table.py
+++ table.py
@@ -105,6 +105,21 @@ class BangTable:
           (name text, address text, city text, state text, postal_code text, phone_number text)"""))
       khan.commit()
 
+   def query_all_customers(all_customers):
+    '''Query and print all of the customers for Bangazon
+
+    Method arguments
+    ----------------
+      n/a
+    '''
+
+    with sqlite3.connect('bangazon.db') as khan:
+      k = khan.cursor()
+
+      k.execute("select * from bangazon where  and symbol=?", ("""create table customer
+          (name text, address text, city text, state text, postal_code text, phone_number text)"""))
+      khan.commit()
+
 
 
 
